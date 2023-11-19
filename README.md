36471eb913d4771752f5103b2ca9b113c52c1948
luana_zamperlini <luazamperlini@gmail.com>
2023-11-19 14:09:10 -0600

    new files created

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Unit2.Froyo
+
+In this project we will be creating an Ice Cream website that receives a prompt upon opening the website for the User to enter a list of comma-separated flavors.
+We should then be able count how many of each flavor the User have typed and list them down.
+
+
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,11 @@
+//Create a function with the prompt method with a message "What's your delicious order?"
+let flavors = prompt("What's your delicious order?");
+//vanilla,vanilla,vanilla,strawberry,coffee,coffee
+
+//Receive the input of flavors the user wishes to order as a string.
+
+//Remove the commas separation from the string input and turn into an array of objects.
+
+// Store the result of this input on a variable named "customerOrder".
+
+//Do a loop through the array and count each flavor inputted.
